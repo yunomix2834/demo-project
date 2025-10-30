fd4e23f8a9debbe0169219077fdb78d2e3ae3f90
Yunom Dev <yunomix2834@gmail.com>
2025-10-30 11:23:12 +0700

    Update

---
+++ demo-gatewayserver/src/main/java/com/gatewayserver/DemoGatewayserverApplication.java
@@ -0,0 +1,13 @@
+package com.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGatewayserverApplication.class, args);
+	}
+
+}
